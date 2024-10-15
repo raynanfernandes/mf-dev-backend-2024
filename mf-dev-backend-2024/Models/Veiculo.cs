2c04191a94e995113157d4cc9d22f4a157c7cc27
Raynan <raynanfernandes15@yahoo.com.br>
2024-10-15 15:36:04 -0300

    Criação dos botões Editar, Vizualizar e Apagar .

--- mf-dev-backend-2024/Models/Veiculo.cs
+++ mf-dev-backend-2024/Models/Veiculo.cs
@@ -15,9 +15,12 @@ namespace mf_dev_backend_2024.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o ano de fabricação!")]
+        [Display(Name = "Ano de fabricação")]
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o ano do modelo!")]
+        [Display(Name = "Ano do modelo")]
+
         public int AnoModelo { get; set; }
     }
 }
